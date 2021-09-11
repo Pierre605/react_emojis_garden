4e96940414a81a019ad891f568e8e68c09f0bc0b
Pierre <Pierre>
2021-09-11 12:47:34 +0200

    Updates

---
+++ precache-manifest.a72d0ab5aa3ec58847ef5995bc493d01.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91a4ab7448b9a25d6dce6edda3f6c49b",
+    "url": "/react_emojis_garden/index.html"
+  },
+  {
+    "revision": "0da6b0d388ac06d00d08",
+    "url": "/react_emojis_garden/static/css/main.1b14ba53.chunk.css"
+  },
+  {
+    "revision": "78ff26926ed460b3bca9",
+    "url": "/react_emojis_garden/static/js/2.14f1dc44.chunk.js"
+  },
+  {
+    "revision": "0da6b0d388ac06d00d08",
+    "url": "/react_emojis_garden/static/js/main.29f07aab.chunk.js"
+  },
+  {
+    "revision": "d969f100c9bd26be80a7",
+    "url": "/react_emojis_garden/static/js/runtime~main.91e733a5.js"
+  }
+]);
